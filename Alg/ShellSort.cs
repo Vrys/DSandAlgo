1180126dadf7a1561bf340fac87f05376d337027
Vladimir Yas <mirsayv@gmail.com>
2017-04-28 23:55:59 +0300

    first fix

---
+++ Alg/ShellSort.cs
@@ -0,0 +1,29 @@
+﻿namespace Alg
+{
+    public class ShellSort
+    {
+        public static void Sort(int[] mas)
+        {
+            int increment = 3;
+            while (increment > 0) 
+            {
+                for (int i = 0; i < mas.Length; i++) 
+                {
+                    int j = i; 
+                    int temp = mas[i];
+
+                    while ((j >= increment) && (mas[j - increment] > temp))
+                    {
+                        mas[j] = mas[j - increment]; 
+                        j = j - increment;
+                    }
+                    mas[j] = temp; 
+                }
+                if (increment > 1)
+                    increment = increment / 2;
+                else if (increment == 1)
+                    break;
+            }
+        }
+    }
+}
---
+++ DS/StackArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DS
+{
+    public class StackArray<T>
+    {
+        T[] array;
+
+        public int Count { get; internal set; }
+
+        public StackArray() : this(0) { }
+
+        public StackArray(int count)
+        {
+            array = new T[count];
+            Count = count;
+        }
+
+        public bool IsEmpty()
+        {
+            return array.Length == 0 ? true : false;
+        }
+
+        void Increase()
+        {
+            int length = array.Length == 0 ? 4 : array.Length << 1;
+            T[] newarray = new T[length];
+            array.CopyTo(newarray, 0);
+            array = newarray;
+        }
+
+        public void Push(T item)
+        {
+            if (Count == array.Length)
+                Increase();
+            array[Count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidCastException("Stack is empty");
+            T item = array[--Count];
+            array[Count] = default(T);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return array[Count - 1];
+        }
+    }
+}
