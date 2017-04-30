047e88ecdd4bc72067c1bea40c9c05ab23310452
Vladimir Yas <mirsayv@gmail.com>
2017-04-30 13:23:37 +0300

    1

---
+++ DS/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Value = data;
+        }
+
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ DS/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        int count;
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = head; 
+            head = node;
+            count++;
+        }
+        public T Pop()
+        {           
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            Node<T> temp = head;
+            head = head.Next; 
+            count--;
+            return temp.Value;
+        }
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            return head.Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+    }
+}
