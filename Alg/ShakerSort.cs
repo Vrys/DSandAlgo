d9e174b6e3c925e438b2324ba2b0d8ff4df1dcad
Vladimir Yas <mirsayv@gmail.com>
2017-05-09 16:47:55 +0300

    1

---
+++ Alg/ShakerSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alg
+{
+    public class ShakerSort
+    {
+        public static void Sort(int[] mass)
+        {
+            int left = 0, right = mass.Length - 1; 
+            int flag = 1;  
+            while ((left < right) && flag > 0)
+            {
+                flag = 0;
+                for (int i = left; i < right; i++)  
+                {
+                    if (mass[i] > mass[i + 1]) 
+                    {             
+                        int t = mass[i];
+                        mass[i] = mass[i + 1];
+                        mass[i + 1] = t;
+                        flag = 1;     
+                    }
+                }
+                right--; 
+                for (int i = right; i > left; i--) 
+                {
+                    if (mass[i - 1] > mass[i]) 
+                    {          
+                        int t = mass[i];
+                        mass[i] = mass[i - 1];
+                        mass[i - 1] = t;
+                        flag = 1;    
+                    }
+                }
+                left++; 
+            }
+        }
+    }
+}
