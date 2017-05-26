87747ff1bf486f24046901fbbcecdae1c9fa81bc
Vladimir Yas <mirsayv@gmail.com>
2017-05-26 19:08:42 +0300

    1

---
+++ DS/CircularDoubleLinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS
+{
+    public class CircularDoubleLinkedList<T> : IEnumerable<T>  
+    {
+        DoublyNode<T> head; 
+        int count;  
+
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+                head.Next = node;
+                head.Previous = node;
+            }
+            else
+            {
+                node.Previous = head.Previous;
+                node.Next = head;
+                head.Previous.Next = node;
+                head.Previous = node;
+            }
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            DoublyNode<T> removedItem = null;
+            if (count == 0) return false;
+
+            do
+            {
+                if (current.Value.Equals(data))
+                {
+                    removedItem = current;
+                    break;
+                }
+                current = current.Next;
+            }
+            while (current != head);
+
+            if (removedItem != null)
+            {
+
+                if (count == 1)
+                    head = null;
+                else
+                {
+       
+                    if (removedItem == head)
+                    {
+                        head = head.Next;
+                    }
+                    removedItem.Previous.Next = removedItem.Next;
+                    removedItem.Next.Previous = removedItem.Previous;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            if (current == null) return false;
+            do
+            {
+                if (current.Value.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            while (current != head);
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            do
+            {
+                if (current != null)
+                {
+                    yield return current.Value;
+                    current = current.Next;
+                }
+            }
+            while (current != head);
+        }
+    }
+}
---
+++ DS/DoubleLinkedList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Value = data;
+        }
+
+        public T Value { get; set; }
+
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        DoublyNode<T> head; 
+        DoublyNode<T> tail; 
+        int count;  
+
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    tail = current.Previous;
+                }
+
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                if (current.Value.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            DoublyNode<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Previous;
+            }
+        }
+    }
+}
